6338e46bbe3f00f093eff47236b96775972296b0
mkoertgen <marcel.koertgen@gmail.com>
2016-10-04 17:42:14 +0200

    Initial commit

---
+++ java/src/test/java/de/mkoertgen/samples/signalr/ProgramTest.java
@@ -0,0 +1,13 @@
+package de.mkoertgen.samples.signalr;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import de.mkoertgen.samples.signalr.Program;
+
+public class ProgramTest {
+    @Test public void testSomeLibraryMethod() {
+        Program classUnderTest = new Program();
+        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
